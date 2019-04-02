1f2c6881fc62e5cc90d5e14d93234bccdfe224cc
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2019-04-02 10:32:02 +0100

    sqlite practical

---
+++ assessment_1/5_sqlite/create_db.py
@@ -0,0 +1,14 @@
+"""
+description:
+Script to create a database for the database practical for advanced programming.
+date: 19/02/28
+@author: ksuchak
+"""
+# Imports
+import sqlite3 as sql3
+
+with sql3.connect('results_db.sqlite') as con:
+    c = con.cursor()
+    c.execute("CREATE TABLE Results (address text, burglaries integer)")
+    c.execute("INSERT INTO Results VALUES ('Queen Vic', 2)")
+    con.commit()
---
+++ assessment_1/5_sqlite/read_db.py
@@ -0,0 +1,14 @@
+"""
+description:
+A script to read the database for the database practicals for advanced
+programming.
+date: 19/02/28
+@author: ksuchak
+"""
+# Imports
+import sqlite3 as sql3
+
+with sql3.connect('results_db.sqlite') as con:
+    c = con.cursor()
+    for row in c.execute("SELECT * FROM Results ORDER BY burglaries"):
+        print("{0}\t{1}".format(row[0], row[1]))
