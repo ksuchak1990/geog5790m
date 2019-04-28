7f86cfc4478ab28fb31f3074380430f186ccdaf3
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2019-04-28 21:44:41 +0100

    comments for sqlite

--- assessment_1/5_sqlite/create_db.py
+++ assessment_1/5_sqlite/create_db.py
@@ -9,6 +9,7 @@ import sqlite3 as sql3
 
 with sql3.connect('results_db.sqlite') as con:
     c = con.cursor()
+    # create a table and insert an entry
     c.execute("CREATE TABLE Results (address text, burglaries integer)")
     c.execute("INSERT INTO Results VALUES ('Queen Vic', 2)")
     con.commit()
--- assessment_1/5_sqlite/read_db.py
+++ assessment_1/5_sqlite/read_db.py
@@ -10,5 +10,6 @@ import sqlite3 as sql3
 
 with sql3.connect('results_db.sqlite') as con:
     c = con.cursor()
+    # print out each row ordered by burglaries
     for row in c.execute("SELECT * FROM Results ORDER BY burglaries"):
         print("{0}\t{1}".format(row[0], row[1]))
