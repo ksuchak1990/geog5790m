747a3584ccfe27eff4180ffe822676cece979284
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2019-05-05 22:18:33 +0100

    produce observation data

--- assessment_2/src/main.py
+++ assessment_2/src/main.py
@@ -6,6 +6,7 @@ A python script to test out the Ensemble Kalman Filter with a basic model.
 
 # Imports
 import matplotlib.pyplot as plt
+import numpy as np
 from Model import Car_Model
 
 # Constants
@@ -14,6 +15,8 @@ CAR_Y_SPEED = 5
 N_STEPS = 100
 MODEL_NOISE_MEAN = 0
 MODEL_NOISE_STD = 10
+OBS_NOISE_MEAN = 0
+OBS_NOISE_STD = 10
 
 def make_data(params, n, vis=True):
     """
@@ -45,3 +48,21 @@ track_params = {'x_speed': CAR_X_SPEED,
                 'noise_std': 0}
 
 true_x, true_y, true_times = make_data(track_params, N_STEPS)
+
+# Use model to make 'observation' data
+obs_params = {'x_speed': CAR_X_SPEED,
+              'y_speed': CAR_Y_SPEED,
+              'noise_mean': 0,
+              'noise_std': 0}
+
+obs_x, obs_y, obs_times = make_data(obs_params, N_STEPS, vis=False)
+
+# Add noise to observations
+obs_x = [x + np.random.normal(OBS_NOISE_MEAN, OBS_NOISE_STD) for x in obs_x]
+obs_y = [y + np.random.normal(OBS_NOISE_MEAN, OBS_NOISE_STD) for y in obs_y]
+
+plt.figure()
+plt.scatter(obs_x, obs_y)
+plt.xlabel('Observed car x-position')
+plt.ylabel('Observed car y-position')
+plt.show()
